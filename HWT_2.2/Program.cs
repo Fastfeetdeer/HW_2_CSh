c4574fc1970ccac4ec2054817d04c7c4f9a6c297
e.demin <demsq@mail.ru>
2023-02-06 14:27:09 +0300

    Start Task 2.2

---
+++ HWT_2.2/Program.cs
@@ -0,0 +1,13 @@
+﻿//ДЗ 2.2 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+string n = a.ToString();
+int len = n.Length;
+if (len > 2)
+{
+    Console.WriteLine(n[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
