4480cf83ac119249ddf9d2dfd28fa8093dac29dc
e.demin <demsq@mail.ru>
2023-02-06 14:23:33 +0300

    Task 2.1 edit

---
+++ HWT_2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// ДЗ 2.1 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Ввидите число");
+int a = int.Parse(Console.ReadLine());
+if (99 < a && a < 1000)
+{
+    int res = a / 10 % 10;
+    Console.WriteLine(res);      
+}
+ else 
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
